7b657a43ae4e632b544cb83ab39043e6593925d1
Matt Matteson <matt@mattmatteson.com>
2015-11-11 15:42:29 -0800

    remove followers attribute from candidates model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111233520) do
+ActiveRecord::Schema.define(version: 20151111234210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20151111233520) do
     t.string   "name"
     t.string   "party"
     t.string   "twitter_url"
-    t.integer  "followers"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "twitter_handle"
