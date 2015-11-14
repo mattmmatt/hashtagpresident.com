590e811ddd8c1ee820f760825d2da51e11d77850
Matt Matteson <matt@mattmatteson.com>
2015-11-13 18:59:29 -0800

    percet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,7 +17,7 @@ class WelcomeController < ApplicationController
       ch[:"Followers Count"] = number_with_delimiter(lastCount.twitter_followers)
       count24HoursAgo = c.followerCounts.where("created_at >= ?", lastCount.created_at - 24.hours).first
       hourCountChange = lastCount.twitter_followers - count24HoursAgo.twitter_followers
-      percent24HoursAgo = number_to_percentage(hourCountChange / lastCount.twitter_followers, precision: 2)
+      percent24HoursAgo = number_to_percentage(hourCountChange.to_f / lastCount.twitter_followers, precision: 2)
       ch[:"24change"] = number_with_delimiter(hourCountChange) + " (" + percent24HoursAgo + ")"
       r.push(ch)
     end
